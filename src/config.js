75bca9aa0c4e0522f6d5e910d5cbc313e3234cd8
Swastik <swastik029290@gmail.com>
2024-05-05 18:18:52 +0530

    Initial Commit

--- src/config.js
+++ src/config.js
@@ -1,17 +1,17 @@
 const config = {
     s3: {
       REGION: "us-east-1",
-      BUCKET: "biswajit-notes-app-file-uploads",
+      BUCKET: "notes-api-bucket-04052024",
     },
     apiGateway: {
       REGION: "us-east-1",
-      URL: "https://gt8y8d48c3.execute-api.us-east-1.amazonaws.com/prod",
+      URL: "https://r8b5vqgi0g.execute-api.us-east-1.amazonaws.com/prod",
     },
     cognito: {
       REGION: "us-east-1",
-      USER_POOL_ID: "us-east-1_AprziSqbJ",
-      APP_CLIENT_ID: "4ruv0hmm39tkl5idrpdkhukfl",
-      IDENTITY_POOL_ID: "us-east-1:b42bed9c-2689-4d7f-9107-e03e45967335",
+      USER_POOL_ID: "us-east-1_apX5R9wmO",
+      APP_CLIENT_ID: "5o85av7tvjts1ubv42hofanoet",
+      IDENTITY_POOL_ID: "us-east-1:c3d6d860-a74b-4181-a08e-eb4a74b08fd8",
     },
   };
   export default config;
